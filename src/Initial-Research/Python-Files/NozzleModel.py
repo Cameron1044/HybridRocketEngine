9b5df2fe1bfc7ceba29a33782f175ced65c65c66
Jacob-VerMeer <jacobvermeer52@gmail.com>
2023-10-02 16:05:50 -0600

    Adding comments, starting code for thrust section

--- src/Initial-Research/Python-Files/NozzleModel.py
+++ src/Initial-Research/Python-Files/NozzleModel.py
@@ -8,10 +8,14 @@ import math as m
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
+# Constants
+gc = 32.2 # [ft/s^2] - Gravitational constant
+Pa = 0 # Ambient Pressure
 mp = 0
 C_str = 0
 Pc = 0
 mo = 0
+mdot = mo + mp
 AeoAt = 3.31
 alpha = 15 # [Degrees] - typical half cone angle for majority of nozzle designs
 
@@ -19,13 +23,18 @@ alpha = 15 # [Degrees] - typical half cone angle for majority of nozzle designs
 def Nozzle_Characteristics():
     At = ((mp+mo)*C_str)/Pc # Calculates optimal throat area given mass flow rate, characteristic velcoity, and chamber pressure
     rt = np.sqrt(At/np.pi) # Determines throat radius based on throat area
-    Ae = AeoAt * At # Calculates exit area w/ throat area and area ratio
+    Ae = AeoAt * At # Calculates exit area w/ throat area and area ratio (given constants)
     re = np.sqrt(Ae/np.pi) # Determines exit radius based on exit area
 
     # Calculate nozzle length
     Dt = 2 * rt # Throat Diameter
     De = 2 * re # Exit Diameter
-    Length_cone = (1/2 * (De - Dt)) / (np.tan(alpha))
+    Length_cone = (1/2 * (De - Dt)) / (np.tan(alpha)) # Calculate cone length
 
-    return At, rt, Length_cone
+    return At, rt, Length_cone, Ae
 
+# Function to predict thrust performance w/ nozzle geometry
+def CalculateThrust(Ve, Ae, Pe):
+    T = (mdot * Ve / gc) + ((Pe - Pa) * Ae)
+ # Currently missing Pe, the exit pressure at the end of the nozzle. May very well be a problem like the nozzle problems from aero :/
+    return T
