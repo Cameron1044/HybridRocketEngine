f77d96dfdfc3b1b57a15b5290e17563635c22e84
Jacob-VerMeer <jacobvermeer52@gmail.com>
2023-10-02 15:49:01 -0600

    Completed Nozzle Geometry Function Definition

--- src/Initial-Research/Python-Files/NozzleModel.py
+++ src/Initial-Research/Python-Files/NozzleModel.py
@@ -12,10 +12,20 @@ mp = 0
 C_str = 0
 Pc = 0
 mo = 0
+AeoAt = 3.31
+alpha = 15 # [Degrees] - typical half cone angle for majority of nozzle designs
 
+# Function to define nozzle geometry:
 def Nozzle_Characteristics():
-    At = ((mp+mo)*C_str)/Pc
+    At = ((mp+mo)*C_str)/Pc # Calculates optimal throat area given mass flow rate, characteristic velcoity, and chamber pressure
+    rt = np.sqrt(At/np.pi) # Determines throat radius based on throat area
+    Ae = AeoAt * At # Calculates exit area w/ throat area and area ratio
+    re = np.sqrt(Ae/np.pi) # Determines exit radius based on exit area
 
+    # Calculate nozzle length
+    Dt = 2 * rt # Throat Diameter
+    De = 2 * re # Exit Diameter
+    Length_cone = (1/2 * (De - Dt)) / (np.tan(alpha))
 
-    return
+    return At, rt, Length_cone
 
