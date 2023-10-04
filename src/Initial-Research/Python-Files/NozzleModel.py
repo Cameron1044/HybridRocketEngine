807e4eb9f81bab73e59877b22d2de11970131c9b
Zodac11 <krivigupta@gmail.com>
2023-10-03 18:38:53 -0600

    added more equations. Need to verify if Pe=Pa!!

--- src/Initial-Research/Python-Files/NozzleModel.py
+++ src/Initial-Research/Python-Files/NozzleModel.py
@@ -7,29 +7,34 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 # Constants
+gamma = 1.4 #change to correct gamma
 gc = 32.2 # [ft/s^2] - Gravitational constant ONLY USE IF NOT WORKING IN SI UNITS
 Pa = 0 # Ambient Pressure
 mp = 0 #mass flow of propellant 
 mo = 0 #mass flow of oxidizer
-C_str = 0
-Pc = 0 #Chamber Pressure
+C_str = 0 #characteristic velocity
+Pc = 0 #Chamber Pressure and can be total pressure Pt for nozzle stations
 mdot = mo + mp #mass flow of oxidizer and propellant
 AeoAt = 3.31 #Area ratio
 alpha = 15 # [Degrees] - typical half cone angle for majority of nozzle designs
+Pe = 0 #exit pressure assumed to be ambient pressure for perfectly expanded nozzle
+Tc = 0; #chamber temperature same as total temperature Tt in nozzle
 
 # Function to define nozzle geometry:
-def Nozzle_Characteristics(mdot,C_str,Pc,AeoAt,alpha):
+def Nozzle_Characteristics(mdot,C_str,Pc,Pe,AeoAt,alpha):
     At = (mdot*C_str)/Pc # Calculates optimal throat area given mass flow rate, characteristic velcoity, and chamber pressure
     rt = np.sqrt(At/np.pi) # Determines throat radius based on throat area
-    Ae = AeoAt * At # Calculates exit area w/ throat area and area ratio (given constants)
     re = np.sqrt(Ae/np.pi) # Determines exit radius based on exit area
+    Ae = AeoAt * At # Calculates exit area w/ throat area and area ratio (given constants)
 
     # Calculate nozzle length
     Dt = 2 * rt # Throat Diameter
     De = 2 * re # Exit Diameter
-    Length_cone = (1/2 * (De - Dt)) / (np.tan(alpha)) # Calculate cone length
-
-    return At, rt, Length_cone, Ae
+    Length_cone = (0.5 * (De - Dt)) / (np.tan(alpha)) # Calculate cone length
+    Me = np.sqrt((2/(gamma-1))*((Pc/Pe)**((gamma-1)/gamma)-1))
+    Te = Tc/(1+((gamma-1)/2)*(Me**2)) #exit temperature 
+    Pthr = Pc*(2/(gamma+1))**(gamma/(gamma-1)) #Pressure at throat of nozzle assuming Mach = 1 at throat
+    return At, rt, Length_cone, Ae,Dt,De,Length_cone,Me,Te,Pthr
 
 # Function to predict thrust performance w/ nozzle geometry
 def CalculateThrust(mdot,Ve, Ae, Pe):
