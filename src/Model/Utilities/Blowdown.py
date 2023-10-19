20ca13ba5ad2b033f80ddeccb1e86ba45ea10677
Cameron1044 <cameronpelkey.808@gmail.com>
2023-10-19 13:21:23 -0600

    setup base for two models

---
+++ src/Model/Utilities/Blowdown.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math as m
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+class BlowdownModel():
+    def __init__(self, input1=0):
+        self.input1 = input1
+
+        
+    def equation1(self):
+        number = self.input1**2
+        return dP_dt
+
+    def equation1(self):
+        
+
+    def equation1(self):
+
+    def combustionModel(self):
+        dP_dt = self.equation1()
---
+++ src/Model/Utilities/Combustion.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math as m
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+class CombustionModel():
+    def __init__(self):
+        
+    def equation1(self):
+
+    def equation1(self):
+
+    def equation1(self):
+
+    def combustionModel(self):
