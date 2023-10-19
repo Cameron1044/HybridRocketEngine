939609635269106590f6fdc22ae98cc352d8058e
KringKong <chriskg2001@gmail.com>
2023-10-19 16:36:52 -0600

    ToMetric and ToEnglish added for Unit Conversion

--- src/Model/Utilities/utilities.py
+++ src/Model/Utilities/utilities.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def units(value, conversion, n=1):
+def ToMetric(value, conversion, n=1):
     ## To take in a value and the current unit it is in to change it into metric
     ## INPUTS: value - the value we want to convert
     ##         unit  - the unit that will be changed to metric
@@ -27,7 +27,12 @@ def units(value, conversion, n=1):
         # Force --> N
         "lbf": 4.448,
         # Density --> kg/m^3
+        "lbm/in^3": 27680,
         "lbm/ft^3": 16.01846337396,
+        # Molecular Weight --> lb/lbmo
+        "lb/lbmol": 1/1000,
+        # Temperature --> K
+        "R": 5/9 ,
 
         ## For Units that are Metric
         # Length --> m
@@ -56,6 +61,7 @@ def units(value, conversion, n=1):
         "kg/m^3": 1,
         # Molecular Weight --> kg/mol
         "g/mol": 1/1000,
+        "kg/mol": 1,
         # Temperature --> K
         "K": 1,
         # Gas Constant --> J/(mol*K)
@@ -66,5 +72,74 @@ def units(value, conversion, n=1):
 
         ## For a Burn coefficient
         "a": (0.0254**(1 + 2*(n)))*(0.453592**(-n))
+    }
+    return value * conversionDict[conversion]
+
+def ToEnglish(value, conversion):
+    ## To take in a value and the current unit it is in to change it into metric
+    ## INPUTS: value - the value we want to convert
+    ##         unit  - the unit that will be changed to metric
+    ##         n     - Fuel Regression constant used to calculate a
+    ##
+    ## OUTPUTS: The Value will have it's units changed into metric / SI units
+    ##          If the unit is already in metric, nothing will happen
+    conversionDict = {
+        ## For Units that are English
+        # Length --> in
+        "in": 1,
+        "ft": 1/12,
+        # Surfrace Area --> in2
+        "in^2": 1,
+        "ft^2": 1/144,
+        # Volume --> in3
+        "in^3": 1,
+        "ft^3": 1/1728,
+        # Pressure --> Psi
+        "psi": 1,
+        # Mass --> lbm
+        "lbm": 1,
+        # Force --> lbf
+        "lbf": 1,
+        # Density --> lbm/in^3
+        "lbm/in^3": 1,
+        "lbm/ft^3": 1/ 1728,
+        # Molecular Weight --> lb/lbmol
+        "lb/lbmol": 1,
+        # Temperature --> R
+        "R": 1,
+
+        ## For Units that are Metric
+        # Length --> in
+        "m": 39.37,
+        "cm": 1/2.54,
+        "mm": 1/25.4,
+        # Surfrace Area --> in2
+        "m^2": 1550,
+        "cm^2": 1/ 6.452,
+        "mm^2": 1 / 645.2,
+        # Volume --> in3
+        "m^3": 61020,
+        "cm^3": 1/16.387,
+        "mm^3": 1/16390,
+        "L": 61.024,
+        # Pressure --> Psi
+        "Pa": 1 / 6895,
+        "kPa": 1 / 6.895,
+        # Mass --> lbm
+        "kg": 2.205,
+        "g": 1/453.6,
+        # Force --> lbf
+        "N": 4.448,
+        "kN": 224.8,
+        # Density --> lbm/in^3
+        "kg/m^3": 1 / 27680,
+        # Molecular Weight --> lb/lbmole
+        "g/mol": 1000,
+        "kg/mol":1,
+        # Temperature --> R
+        "K": 1.8,
+
+        ## Unitless
+        "unitless": 1,
     }
     return value * conversionDict[conversion]
