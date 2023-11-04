a4b0654cff9ecb55582426c1efef4c293dda0cbd
Jacob-VerMeer <jacobvermeer52@gmail.com>
2023-11-04 13:14:39 -0600

    Krivi Nozzle Handcalc in Code, testing Summerfield

---
+++ src/Model/NozzleTesting.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+import math as m
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+## Inputs/Givens
+
+# Pressures
+Pc =  700 # Pressure of the Combustion Chamber [psi] 
+Pc_arr = np.linspace(Pc, 280)
+P0 = Pc # Since flow is essentially stagnant in combustion chamber, we consider this the total pressure of the flow
+Pamb = 14.93 # Ambient pressure at launch site (Fort Collins, CO)
+P0oPe = P0/Pamb
+
+# Geometry
+Dt = 1 # Diameter of the Throat [in]
+At = np.pi/4 * Dt**2 # Area of the Throat (Calculated) [in^2]
+
+# Misc
+gamma = 1.205 # Ratio of speicifc heats
+
+## Calculate all the things 
+
+# Calculate exit mach number from pressure ratio
+Me = np.sqrt( ( (P0oPe) ** ((gamma-1)/gamma) - 1) / ( (gamma - 1) / 2) )
+print("Mach at Nozzle exit is", Me, ", that's pretty fast!")
+
+# Armed with exit Mach number, calcualte area ratio and exit area 
+Arat = np.sqrt((1/Me**2)*((2/(gamma + 1))*(1 + (gamma-1)*Me**2/2))**((gamma+1)/(gamma-1))) 
+Ae = Arat * At
+print("Correct Nozzle Area Ratio is", Ae)
+De = np.sqrt(4*Ae/np.pi)
+print("Correct Nozzle Exit Diameter is", De)
+
+# Ratio of P0 to Pe is PRESERVED regardless of how P0 changes, just a quirk of having choked flow
+Pe_arr = []
+for pressures in Pc_arr:
+    Pe_arr.append((P0oPe)**(-1) * pressures)
+
+PeoPamb = []
+for exitpressures in Pe_arr:
+    PeoPamb.append(exitpressures/Pamb)
+
+print(PeoPamb)
